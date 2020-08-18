00b146bc259b8f1121e6202a4d1e2b9ca3e9db3f
Emilie-dev <chauvin-e@orange.fr>
2020-08-18 16:46:17 +0200

    create profile.show + display username

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-            <h1>{{ }}</h1>
+                <h1>{{ $user->username }}</h1>
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
@@ -22,10 +22,10 @@
     </div>
     <div class="row pt-5">
         <div class="col-4">
-            <img src="img/img-posts/img-post-1.png" class="w-100"/>
+            <img src="/img/img-posts/img-post-1.png" class="w-100"/>
         </div>
-        <div class="col-4"><img src="img/img-posts/img-post-2.png" class="w-100" /></div>
-        <div class="col-4"><img src="img/img-posts/img-post-3.png" class="w-100"/></div>
+        <div class="col-4"><img src="/img/img-posts/img-post-2.png" class="w-100" /></div>
+        <div class="col-4"><img src="/img/img-posts/img-post-3.png" class="w-100"/></div>
     </div>
 </div>
 @endsection
