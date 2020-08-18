49197b2ccef1ec383f4f039bf25e21d6b9ed8181
Emilie-dev <chauvin-e@orange.fr>
2020-08-18 16:25:14 +0200

    create ProfilesController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>laravel-instagram</h1>
+            <h1>{{ }}</h1>
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
