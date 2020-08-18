2d34711ffb45a84595bcd572767fd250de0c4bd4
Emilie-dev <chauvin-e@orange.fr>
2020-08-18 02:21:46 +0200

    add logo + started frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="row">
+        <div class="col-3 p-3">
+            <img src="/img/laravel-instagram-logo-full.png" style="height: 150px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>laravel-instagram</h1>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>153</strong> posts</div>
+                    <div class="pr-5"><strong>23k</strong> followers</div>
+                    <div class="pr-5"><strong>212</strong> following</div>
                 </div>
             </div>
+            <div class="pt-4 font-weight-bold">laravel-instagram.com</div>
+            <div>Welcom to laravel-instagram.</div>
+            <div><a href="#">laravel-instagram.com</a></div>
         </div>
     </div>
 </div>
