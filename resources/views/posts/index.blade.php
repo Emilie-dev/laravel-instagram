3ebbb3b67789698c0f78d27a92a67173c93d9927
Emilie-dev <chauvin-e@orange.fr>
2020-08-20 22:05:29 +0200

    style following posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,36 +6,21 @@
     @foreach ($posts as $post)
 
         <div class="row">
-            <div class="col-8">
-                <img src="/storage/{{ $post->image }}"  class="w-100 "/>
+            <div class="col-6 offset-3">
+            <a href="/profile/{{ $post->user->id }}">
+                    <img src="/storage/{{ $post->image }}"  class="w-100 "/>
+                </a>
             </div>
-            <div class="col-4">
-
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-6 offset-3">
                 <div>
-                    <div class="d-flex align-items-center">
-                        <div class="pr-3">
-                            <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-height: 40px;">
-                        </div>
-                        <div>
-                            <div class="font-weight-bold">
-                                <a href="/profile/{{ $post->user->id }}">
-                                    <span class="text-dark">{{ $post->user->username }}</span>
-                                </a>
-
-                                <a href="#" class="pl-3">Follow</a>
-
-                            </div>
-                        </div>
-                    </div>
+                    <p><span class="font-weight-bold">
+                        <a href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a>
+                    </span> {{ $post->caption }}</p>
                 </div>
-
-                <hr>
-
-                <p><span class="font-weight-bold">
-                    <a href="/profile/{{ $post->user->id }}">
-                        <span class="text-dark">{{ $post->user->username }}</span>
-                    </a>
-                </span> {{ $post->caption }}</p>
             </div>
         </div>
 
