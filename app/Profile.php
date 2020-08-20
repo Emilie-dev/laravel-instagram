49cf970499c5c3bbe6f01e5886a4bb77b07c6574
Emilie-dev <chauvin-e@orange.fr>
2020-08-20 17:24:36 +0200

    add default image profile

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,13 @@ class Profile extends Model
 
     protected $guarded = [];
 
+    public function profileImage() {
+
+        $imagePath = ($this->image) ?  $this->image : 'profile/NRt5WMaNZTsvsZsZQZTVWkx5IcyRdJBdJzQfWXM5.png';
+
+        return '/storage/' . $imagePath;
+    }
+
     public function user() {
         return $this->belongsTo(User::class);
     }
